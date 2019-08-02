ad65598838e74f55ccaa02ac91eedbf553336d60
ChanukaAthalage <chanukaathalage@gmail.com>
2019-08-02 17:41:16 +0530

    first commit

--- app/src/main/java/com/example/bybus/passenger_profile.java
+++ app/src/main/java/com/example/bybus/passenger_profile.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Main3Activity extends AppCompatActivity {
+public class passenger_profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.passenger_profile);
     }
 
 
